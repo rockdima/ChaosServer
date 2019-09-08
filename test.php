f251a6041b73e2506a0e614c07356be4cd272363
rockdima <55030630+rockdima@users.noreply.github.com>
2019-09-08 10:28:44 +0300

    added comments

--- test.php
+++ test.php
@@ -2,25 +2,28 @@
 
 error_reporting(E_ERROR | E_PARSE);
 
-$percents = [];
 
-$requests_qty = 10;
+$percents = []; //response qty by code
+$requests_qty = 100;
 
+//loop requests
 for ($i = 0; $i < $requests_qty; $i++) {
 
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'http://chaosserver/response');
+	curl_setopt($ch, CURLOPT_URL, 'http://chaosserver/response'); //need to config your apache or nginx
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 1);
 	$result = curl_exec($ch);
 	curl_close($ch);
 
+	//get response code
 	$code = substr($result, 9, 3);
 
+	//set a code and increase 
 	$percents[$code]++;
 
 }
-
+//show the real percentage
 foreach($percents as $code=>$qty) {
 	echo "{$code}: ".ceil($qty/$requests_qty*100).'%<br>';
 }
